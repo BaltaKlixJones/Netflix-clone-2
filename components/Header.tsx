172ae7523cd972d63770c9be004f4b8724e0c9dd
BaltaKlixJones <balta98_@hotmail.com>
2023-04-25 10:38:04 -0300

    Inital commit

---
+++ components/Header.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+import { AiOutlineSearch, AiOutlineBell } from 'react-icons/ai';
+import {BsBellFill} from 'react-icons/bs'
+import { useState, useEffect } from 'react';
+
+function Header() {
+
+    const [isScroll, setIsScroll] = useState(false)
+
+    useEffect(() => {
+
+    },[])
+
+    
+  return (
+    <header>
+    <div className="flex  items-center space-x-2 md:space-x-10">
+    <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+            <li className="headerLink">Home</li>
+            <li className="headerLink">Tv Show</li>
+            <li className="headerLink">Movies</li>
+            <li className="headerLink">New & Popular</li>
+            <li className="headerLink">My List</li>
+        </ul>
+    </div>
+    <div className='flex items-center space-x-4 text-sm font-light'>
+        <AiOutlineSearch className='hidden h-6 w-6 sm:inline' />
+        <p className='hidden lg:inline'>Kids</p>
+        <BsBellFill className='h-5 w-5'/>
+        <Link href="/account">
+        <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+    </div>
+    </header>
+  )
+}
+
+export default Header
